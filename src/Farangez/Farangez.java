6dcfa649b98c6b2cfc1cdcc97a02414863e78d03
Farangez Hikmatova <Fhikmatova@gmail.com>
2022-01-24 21:46:04 -0500

    added print statment to Farangez class

--- src/Farangez/Farangez.java
+++ src/Farangez/Farangez.java
@@ -3,5 +3,8 @@ package Farangez;
 public class Farangez {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        System.out.println("Hello everyone");
+
     }
 }
